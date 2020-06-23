d9ddfe14d99adf3c9bc541883b3e88c6334e27bb
programmer-joe <66311244+programmer-joe@users.noreply.github.com>
2020-06-23 08:20:27 -0700

    changes

--- db_main.py
+++ db_main.py
@@ -46,6 +46,31 @@ jobEntryTabOne.grid(row = 2,column = 1, padx = 15,pady =15)
 imgLabelTabOne.grid(row = 0,column =2 , rowspan = 3,pady = 15)
 
 #ADDING WIDGETS FOR TAB2
+firstLabelTabTwo = tk.Label(tab2, text = 'FirstName')
+familyLabelTabTwo = tk.Label(tab2,text = 'Family Name')
+jobLabelTabTwo = tk.Label(tab2,text = 'Job Title')
+
+firstEntryTabTwo = tk.Entry(tab2)
+familyEntryTabTwo = tk.Entry(tab2)
+jobEntryTabTwo = tk.Entry(tab2)
+
+imageLabelTabTwo = tk.Label(tab2)
+
+buttonCommit = tk.Button(tab2, text = 'Add Record To Database')
+buttonAddImage = tk.Button(tab2,text = 'Add Image')
+
+#ADD WIDGET ON TAB TWO
+firstLabelTabTwo.grid(row= 0,column=0,padx=15,pady=15)
+firstEntryTabTwo.grid(row= 0,column = 1,padx =15,pady=15)
+
+familyLabelTabTwo.grid(row =1,column = 0,padx = 15,pady =15)
+familyEntryTabTwo.grid(row = 1,column = 1,padx = 15, pady =15)
+
+jobLabelTabTwo.grid(row = 2,column = 0 , padx = 15,pady = 15)
+jobEntryTabTwo.grid(row = 2,column = 1, padx = 15,pady =15)
+
+buttonCommit.grid(row = 4,column = 1,padx=15,pady=15)
+buttonAddImage.grid(row =4,column = 2,padx=15,pady =15)
 
 tab_parent.pack(expand = 1, fill = 'both')
 
